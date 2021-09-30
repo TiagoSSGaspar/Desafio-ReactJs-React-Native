dacc7b9bb2e780cb84810e10e357c2a294a03bcf
Tiago souza silva gaspar <tiagosouzasilvagaspar@MacBook-Pro-de-Tiago.local>
2021-09-29 22:22:12 -0300

    :sparkles: componentes reutilizáveis, Button, Tooltip, input.  :construction: página Home. :heavy_plus_sign:  polished, styled-components, @unform/web, @unform/core react-icons.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Home: React.FC = () => {
+
+  return (
+    <Container>
+      <h1>Hello World</h1>
+    </Container>
+  );
+};
+
+export default Home;
